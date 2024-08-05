7399a0e8da97e4e3ee0d8c10c256f9ac0bc18557
HENNING KÖNITZ <koenitz92@gmx.de>
2024-08-05 16:02:52 +0200

    GET /products

--- server.js
+++ server.js
@@ -3,8 +3,19 @@ const express = require("express");
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   return res.json({ message: "Welcome to the Koala API" });
+});
+
+app.get("/products", (req, res) => {
+  try {
+    res.json(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
 });
 
 app.listen(port, () => {
