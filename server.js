9484cb03474609f1efba48ee33b6f2be7fb71042
Oleksii Butrin <bootreen@gmail.com>
2024-08-06 15:00:54 +0200

    Implemented route POST /users/login and associated sql-queries

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const port = 3000;
 const app = express();
 const products = require("./data/products");
+const sql = require("@vercel/postgres");
+const SQL_QUERIES = require("./data/sql-queries");
+const { GET_USER, INSERT_NEW_USER, LOGIN_USER } = SQL_QUERIES;
 
 app.use(cors());
 
@@ -16,6 +19,29 @@ app.get("/", (req, res) => {
 app.get("/products", (req, res) => {
   try {
     res.json(products);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+
+// If user exists, updates last_login date/time and returns user object
+// If user doesn't exist, creates a new user and returns user object
+app.post("/users/login", async (req, res) => {
+  const { email, name } = req.body;
+  try {
+    const { rows: existedUser } = await sql.query(GET_USER, [null, email]);
+    if (existedUser) {
+      const { id } = existedUser;
+      const { rows: loggedUser } = await sql.query(LOGIN_USER, [id]);
+      res.status(200).json(loggedUser[0]);
+    } else {
+      const { rows: createdUser } = await sql.query(INSERT_NEW_USER, [
+        email.trim(),
+        name.trim(),
+      ]);
+      res.status(201).json(createdUser[0]);
+    }
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Server error" });
