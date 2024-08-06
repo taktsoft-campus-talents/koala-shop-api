4f5fd7f5bbfbec5172af20021218c67dbfb760f0
Ralf Siewert <ralf@actyvy.st>
2024-08-06 11:22:18 +0200

    fix: prices in cent

--- data/create-products.sql
+++ data/create-products.sql
@@ -1,4 +1,4 @@
-DROP TABLE koala_products;
+DROP TABLE IF EXISTS koala_products;
 
 CREATE TABLE koala_products
 (
@@ -15,27 +15,27 @@ INSERT INTO koala_products
 VALUES
  ('Laptop',
  'HP EliteBook 840 G6 14inches Core i5 1.6 GHz - SSD 256 GB - 16GB QWERTZ - Deutsch',
- '2596.00',
+ 259600,
  'laptop.jpg',
   'tech', 
-  '30');
+  30);
 
   INSERT INTO koala_products 
   (title, description,price,image,category,leftInStock ) 
   VALUES 
   ('Portable Projector','
   Mini Projector, ClokoWe, 1080P HD, 7000L, Compatible with Android/iOS/Windows/TV Stick/HDMI/USB',
-  '159',
+  15900,
   'projector.jpg',
    'tech', 
-'50');
+50);
 
 INSERT INTO koala_products 
 (title, description,price,image,category,leftInStock ) 
 VALUES 
 ('Bluetooth Speaker',
 'DOSS SoundBox Pro+ Bluetooth Speaker with IPX6 Waterproof, 24 W Dual Bass Drivers, Stereo Pairing, Colourful Light',
-'159','speaker.jpg', 'tech', '100');
+15900,'speaker.jpg', 'tech', 100);
 
 INSERT INTO koala_products 
 (title, description,price,image,category,leftInStock )
@@ -43,10 +43,10 @@ INSERT INTO koala_products
  ('Koala Mug','Capacity: 325 ml
 Colored throughout (colored inside and outside)
 Please wash by hand
-Material: 100% ceramic (glossy), ','21','mug.jpg', 'home', '300');
+Material: 100% ceramic (glossy), ',2100,'mug.jpg', 'home', 300);
 
 INSERT INTO koala_products 
 (title, description,price,image,category,leftInStock ) 
 VALUES 
 ('Koala T-Shirt','Retro Sunset Koala Bear Silhouette T-Shirt '
-,'19','tshirt.jpg', 'home', '300');
+,1900,'tshirt.jpg', 'home', 300);
