5134153efadb669e385526ea506f31f5eb6620cb
Gladys Sobrido Sambade <gladys@MacBook-Pro-von-Gladys.local>
2024-08-06 14:23:18 +0200

    db

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const express = require("express");
 const cors = require("cors");
 const port = 3000;
 const app = express();
-const products = require("./data/products");
-
 app.use(cors());
-
 app.use(express.json());
+// const products = require("./data/products");
+const { sql } = require("@vercel/postgres");
 
 app.get("/", (req, res) => {
   return res.json({ message: "Welcome to the Koala API" });
 });
 
-app.get("/products", (req, res) => {
+app.get("/products", async (req, res) => {
+  const { rows: products } = await sql`SELECT * FROM koala_products`;
+
   try {
     res.json(products);
   } catch (err) {
