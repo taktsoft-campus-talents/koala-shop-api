7e5e385b2c9919f74e9885ae6d5b7e844f429994
Oleksii Butrin <bootreen@gmail.com>
2024-08-07 11:58:56 +0200

    Rolled back to much simplier MVP-version

--- data/create-offers.sql
+++ data/create-offers.sql
@@ -3,14 +3,6 @@ DROP TABLE IF EXISTS koala_offers;
 CREATE TABLE koala_offers
 (id SERIAL PRIMARY KEY,
 product_id INTEGER,
-user_id INTEGER,
-type INTEGER NOT NULL,
-value INTEGER,
-valid_from TIMESTAMP,
-valid_to TIMESTAMP,
 CONSTRAINT fk_product
   FOREIGN KEY(product_id)
-  REFERENCES koala_products(id),
-CONSTRAINT fk_user
-  FOREIGN KEY(user_id)
-  REFERENCES koala_users(id));
+  REFERENCES koala_products(id));
