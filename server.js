1e73412e96f6b75ab9ebec2416138f54ac293c59
HENNING KÖNITZ <koenitz92@gmx.de>
2024-08-07 12:57:13 +0200

    add query parameters to /products

--- server.js
+++ server.js
@@ -16,10 +16,43 @@ app.get("/", (req, res) => {
   return res.json({ message: "Welcome to the Koala API" });
 });
 
+// app.get("/products", async (req, res) => {
+//   const { rows: products } = await sql`SELECT * FROM koala_products`;
+
+//   try {
+//     res.json(products);
+//   } catch (err) {
+//     console.log(err);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// });
+
 app.get("/products", async (req, res) => {
-  const { rows: products } = await sql`SELECT * FROM koala_products`;
+  // extract parameters from URL
+  const { category, sort, sortOrder } = req.query;
+
+  // SQL command
+  let query = "SELECT * FROM koala_products";
+
+  // a list to save the parameters for the SQL request
+  let queryParams = [];
+
+  // check if there is a category and check which one
+  if (category && (category === "tech" || category === "home")) {
+    // add category condition to the SQL command
+    query += " WHERE category = $1";
+    // value of category (that the user chose) is inserted in the request
+    queryParams.push(category);
+  }
+
+  if (sort) {
+    const orderBy = sortOrder === "ascending" ? "ASC" : "DESC";
+    // add sort condition to the SQL command
+    query += ` ORDER BY ${sort} ${orderBy}`;
+  }
 
   try {
+    const { rows: products } = await sql.query(query, queryParams);
     res.json(products);
   } catch (err) {
     console.log(err);
