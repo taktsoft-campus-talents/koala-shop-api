bb7529888c3f302b4172dccf4295fea65abfa520
Oleksii Butrin <bootreen@gmail.com>
2024-08-07 11:11:59 +0200

    Decided to add validity time window

--- data/create-offers.sql
+++ data/create-offers.sql
@@ -6,6 +6,8 @@ product_id INTEGER,
 user_id INTEGER,
 type INTEGER NOT NULL,
 value INTEGER,
+valid_from TIMESTAMP,
+valid_to TIMESTAMP,
 CONSTRAINT fk_product
   FOREIGN KEY(product_id)
   REFERENCES koala_products(id),
