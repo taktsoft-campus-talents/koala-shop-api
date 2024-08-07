f034b8201a08b5bedf3b6283c697ebbad2c14063
Oleksii Butrin <bootreen@gmail.com>
2024-08-07 10:49:53 +0200

    Created special offers table

---
+++ data/create-offers.sql
@@ -0,0 +1,14 @@
+DROP TABLE IF EXISTS koala_offers;
+
+CREATE TABLE koala_offers
+(id SERIAL PRIMARY KEY,
+product_id INTEGER,
+user_id INTEGER,
+type INTEGER NOT NULL,
+value INTEGER,
+CONSTRAINT fk_product
+  FOREIGN KEY(product_id)
+  REFERENCES koala_products(id),
+CONSTRAINT fk_user
+  FOREIGN KEY(user_id)
+  REFERENCES koala_users(id));
