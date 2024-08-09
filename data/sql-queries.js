47bb3f6ab3543485e34d9f2d193ae050a93ef167
Oleksii Butrin <bootreen@gmail.com>
2024-08-09 14:57:50 +0200

    GET specials now returns complete product columns + productId + offerId

--- data/sql-queries.js
+++ data/sql-queries.js
@@ -5,6 +5,20 @@ const SQL_QUERIES = {
     "INSERT INTO koala_users (name, created_at, last_login) VALUES ($1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING id, name, created_at, last_login",
   LOGIN_USER:
     "UPDATE koala_users SET last_login = CURRENT_TIMESTAMP WHERE id = $1 RETURNING id, name, created_at, last_login",
-  GET_SPECIALS: "SELECT * FROM koala_offers",
+  GET_SPECIALS: `SELECT
+    koala_offers.id AS "offerId",
+    koala_products.id AS "productId",
+    koala_products.title,
+    koala_products.category,
+    koala_products.description,
+    koala_products.image,
+    koala_products.price,
+    koala_products.leftInStock AS "leftInStock"
+      FROM
+        koala_offers
+      INNER JOIN
+        koala_products
+      ON
+        koala_offers.product_id = koala_products.id;`,
 };
 module.exports = SQL_QUERIES;
