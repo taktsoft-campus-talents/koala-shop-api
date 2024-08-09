ac28a15c19d4d6a8d1b1885158143f6375bdc22c
Ralf Siewert <ralf@actyvy.st>
2024-08-09 16:29:04 +0200

    fix: sql statement for GET /users

--- data/sql-queries.js
+++ data/sql-queries.js
@@ -1,6 +1,6 @@
 const SQL_QUERIES = {
   GET_USER:
-    "SELECT id, name, created_at, last_login FROM users WHERE id = $1 OR name = $2",
+    "SELECT id, name, created_at, last_login FROM koala_users WHERE id = $1 OR name = $2",
   INSERT_NEW_USER:
     "INSERT INTO koala_users (name, created_at, last_login) VALUES ($1, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP) RETURNING id, name, created_at, last_login",
   LOGIN_USER:
